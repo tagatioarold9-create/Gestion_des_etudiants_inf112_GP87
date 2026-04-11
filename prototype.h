086714d1c75bffd536e5a9b6892fb4d9da6feef4
michelnwaha10-max <michelnwaha10@gmail.com>
2026-04-11 13:35:28 +0100

    Prototype par michel

---
+++ prototype.h
@@ -0,0 +1,24 @@
+#ifndef PROTOTYPE_H
+#define PROTOTYPE_H
+#include <time.h>
+
+typedef struct Employer {
+ char matricule[10];
+ char name[50];
+ int age;
+ float salaire;
+ int anciennete;
+ char date_admission[11];
+
+ struct Employer *suivant;
+} Employer; 
+
+typedef struct Produit {
+ char name[40];
+ char numero_de_serie[20];
+ double prix;
+ time_t date_peremption;
+ struct Produit *suivant;
+} Produit;
+
+#endif //PROTOTYPE_H
